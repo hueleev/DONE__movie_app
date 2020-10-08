bd8d44b17ca47fe818f5a60e5924bef7c291e3ff
Hyungwon Lee <hueleev@gmail.com>
2020-10-08 17:45:48 +0900

    add puppy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+
+function Puppy({ name , picture }) {
+  return <div>
+    <h1> I like {name}</h1>
+    <img src={picture} />
+  </div>
+}
+
+const puppys = [
+  {
+    name: "puppy",
+    image: "https://images.mypetlife.co.kr/wp-content/uploads/2018/03/07153649/file_23230_coton-de-tulear.jpg"
+  },
+  {
+    name: "puppy",
+    image: "https://cdn.crowdpic.net/list-thumb/thumb_l_F765F72F320703AA2833A5260D71732D.jpg"
+  }]
+
+// component : HTML을 반환하는 함수
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { puppys.map(puppy => <Puppy name={puppy.name} picture={puppy.image}/>)}
     </div>
   );
 }
