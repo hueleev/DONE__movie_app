8b6da0532eec45e6a0c9caa8b9878ef9d8c6c67d
Hyungwon Lee <hueleev@gmail.com>
2020-10-20 13:37:41 +0900

    Updates

---
+++ precache-manifest.4b8ac5389ef657ad163e393e20d7eaa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db7842d53964fdd1565bd9298e103641",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "c7405867bd68bc2381e7",
+    "url": "/movie_app/static/css/main.d9289715.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7405867bd68bc2381e7",
+    "url": "/movie_app/static/js/main.44cca13e.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
